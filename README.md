de383070317aeb081b7d49f4cac39f676360dbb5
Decupe <igwilondubuezesamuel@gmail.com>
2025-12-08 21:38:11 +0000

    added a python module for calculations and updated readme file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # GithubLearning
 learning how to use Github
+this is my first experince, i love it
---
+++ calculator.py
@@ -0,0 +1,18 @@
+count = 0
+num_list = []
+calculator = ('addition', 'subtraction', 'multiplication', 'division', 'average')
+selection = input("Choose an operation: ").lower()
+
+if selection == 'addition':
+    nums_to_add = int(input('How many numbers do you want to add?: '))
+    while count < nums_to_add:
+        num = int(input('Enter a number: '))
+        num_list.append(num)
+        count += 1
+        
+    result = num_list[0]
+    for n in num_list[1:]:
+        result += n
+        
+    print(f"Result of addition = {result}")
+
